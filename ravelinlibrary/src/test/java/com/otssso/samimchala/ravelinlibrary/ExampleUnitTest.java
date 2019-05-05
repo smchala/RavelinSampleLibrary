4e9436568aeff7bcdd09bc680fd3593af6c4568f
Sami Mchala <smchala@hotmail.com>
2019-05-05 12:16:48 +0100

    sm: tidy up, optimising imports

--- ravelinlibrary/src/test/java/com/otssso/samimchala/ravelinlibrary/ExampleUnitTest.java
+++ ravelinlibrary/src/test/java/com/otssso/samimchala/ravelinlibrary/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.otssso.samimchala.ravelinlibrary;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
